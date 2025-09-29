410d7006166c435538f2c75a2bd5eaa74e7b0e76
SaranyaaUK <saranyaajk95@gmail.com>
2025-09-29 15:46:13 +0100

    Add: string and comprehension

---
+++ Practice_2/practical2.py
@@ -0,0 +1,26 @@
+
+
+# List Comprehension
+
+cohort_1 = ['Schivanee', 'Saranya', 'Aiman', 'Ilia', 'Daveraj', 'Panya', 'Bryan', 'Abdul', 'Jibril', 'Maksym', 'Ano']
+better_cohort_1 = [person for person in cohort_1 if len(person) % 2 != 0]
+
+print(better_cohort_1)
+
+# Unicode Characters
+
+emoji = '\u2728'
+
+cohort_1 = ['Schivanee', 'Saranya', 'Aiman', 'Ilia', 'Daveraj', 'Panya', 'Bryan', 'Abdul', 'Jibril', 'Maksym', 'Ano']
+better_cohort = [name + emoji for name in cohort_1 if len(name) % 2 != 0 ]
+print(better_cohort)
+
+# String slicing
+
+name = "Saranya"
+extract = name[3:]
+print(extract)
+
+new_name = "P" + extract
+print(new_name)
+
